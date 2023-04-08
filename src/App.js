a4d466b00af07d53057f2d88d3629c9a73749788
Long <laptoptandt@Longs-Macbookpro.local>
2023-04-08 14:16:04 +0700

    add new router

--- src/App.js
+++ src/App.js
@@ -3,24 +3,19 @@ import React from 'react';
 import Navbar from './components/navbar';
 import RoutesComponent from './Routes';
 import LogIn from './components/login';
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
+    <Router>
       <Navbar />
-
       {/* Home and Log-in */}
       <Routes>
         <Route path='/' element={<RoutesComponent />}></Route>
         <Route path='/login' element={<LogIn />}></Route>
       </Routes >
-    </>
+    </Router>
   );
 }
 
